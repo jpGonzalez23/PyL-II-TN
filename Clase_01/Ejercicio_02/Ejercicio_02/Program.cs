f7b2d682e013246a4eb15d4ebdfe3408f2869a40
Juan Pablo Gonzalez <juanpigonzalez72@gmail.com>
2023-08-30 22:17:59 -0300

    Se agrega ejercicios

---
+++ Clase_01/Ejercicio_02/Ejercicio_02/Program.cs
@@ -0,0 +1,35 @@
+﻿// Ingresar un número y mostrar el cuadrado y el cubo del mismo.
+// Se debe validar que el número sea mayor que cero, caso contrario, mostrar el mensaje: "ERROR. ¡Reingresar número!".
+
+namespace Ejercicio_02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Ejercicio N°2";
+
+            int numero;
+            double cuadrado = 0;
+            double cubo = 0;
+
+            do
+            {
+                Console.WriteLine("Ingrese un numero: ");
+                int.TryParse(Console.ReadLine(), out numero);
+
+                if (numero <= 0)
+                {
+                    Console.WriteLine("ERROR. ¡Reingresar número!");
+                }
+
+            } while (numero <= 0);
+
+            cuadrado = Math.Pow(numero, 2);
+            cubo = Math.Pow(numero, 3);
+
+            Console.WriteLine($"El cuadrado del numero {numero} es: {cuadrado}");
+            Console.WriteLine($"El cubo del numero {numero} es: {cubo}");
+        }
+    }
+}
---
+++ Clase_01/Ejercicio_03/Ejercicio_03/Program.cs
@@ -0,0 +1,52 @@
+﻿// Mostrar por pantalla todos los números primos que haya hasta el número que ingrese el usuario por consola.
+// Validar que el dato ingresado por el usuario sea un número.
+// Volver a pedir el dato hasta que sea válido o el usuario ingrese "salir".
+// Si ingresa "salir", cerrar la consola.
+// Al finalizar, preguntar al usuario si desea volver a operar. Si la respuesta es afirmativa, iterar.
+// De lo contrario, cerrar la consola.
+
+namespace Ejercicio_03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Ejercicio N°3";
+
+            int numero;
+            int contador = 0;
+            int j = 0;
+            int i;
+            int k;
+            int[] lista = new int[100];
+
+            Console.WriteLine("Ingrese numeros de 0 a 100!");
+           
+            numero = Convert.ToInt32(Console.ReadLine());
+            
+            for (k = 1; k <= numero; k++)
+            {
+                contador = 0;
+                for (i = 1; i <= k; i++)
+                {
+                    if (k % i == 0)
+                    {
+                        contador++;
+                    }
+                }
+                if (contador == 2)
+                {
+                    lista[j] = k;
+                    j++;
+                }
+            }
+           
+            Console.WriteLine("Los numeros primos q ingreso son");
+            
+            for (i = 0; i < j; i++)
+            {
+                Console.WriteLine(lista[i]);
+            }
+        }
+    }
+}
