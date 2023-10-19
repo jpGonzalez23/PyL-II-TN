5f8ea87bba2f84d20e6d73c3161496160efe581d
Juan Pablo Gonzalez <juanpigonzalez72@gmail.com>
2023-10-19 18:56:47 -0300

    se modifica la clase 11 y se agrega  la clase 13

---
+++ Clase_13/Ejercicio_I01/Ejercicio_I01/Program.cs
@@ -0,0 +1,30 @@
+﻿using Entidades;
+
+namespace Ejercicio_I01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Torneo<EquipoBasquet> torneoBasquet = new Torneo<EquipoBasquet>("tBasquet");
+            Torneo<EquipoFutbol> torneofutbol = new Torneo<EquipoFutbol>("tFutbol");
+
+            EquipoBasquet e1 = new EquipoBasquet("e1", DateTime.Now);
+            EquipoBasquet e2 = new EquipoBasquet("e2", DateTime.Now);
+
+            EquipoFutbol f1 = new EquipoFutbol("f1", DateTime.Now);
+            EquipoFutbol f2 = new EquipoFutbol("f2", DateTime.Now);
+
+            _ = torneoBasquet + e1;
+            _ = torneoBasquet + e2;
+
+            _ = torneofutbol + f1;
+            _ = torneofutbol + f2;
+
+
+
+            Console.WriteLine(torneoBasquet.Mostrar());
+            Console.WriteLine(torneofutbol.Mostrar());
+        }
+    }
+}
---
+++ Clase_13/Ejercicio_I02/Ejercicio_I02/Program.cs
@@ -0,0 +1,31 @@
+﻿using Entidades;
+
+namespace Ejercicio_I02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Ejercicio_I02 - Clase_13 - Genericos";
+
+            Recibo recibo1 = new Recibo();
+            Factura factura1 = new Factura(123);
+            Factura factura2 = new Factura(456);
+
+            Contabilidad<Factura, Recibo> contabilidad = new Contabilidad<Factura, Recibo>();
+
+            contabilidad += recibo1;
+            contabilidad += factura1;
+            contabilidad += factura2;
+
+            foreach (Documento item in contabilidad.egresos)
+            {
+                Console.WriteLine(item.Numero);
+            }
+            foreach (Documento item in contabilidad.ingresos)
+            {
+                Console.WriteLine(item.Numero);
+            }
+        }
+    }
+}
